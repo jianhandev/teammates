66f968e125b0f6869fad05e63cd8ff69f1b08d9c
jianhandev <jianhanli.dev@gmail.com>
2021-03-25 20:49:00 +0800

    Add functionality to put student and instructor documents in Solr

--- src/main/java/teammates/storage/search/SearchManager.java
+++ src/main/java/teammates/storage/search/SearchManager.java
@@ -1,12 +1,19 @@
 package teammates.storage.search;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient;
+import org.apache.solr.common.SolrInputDocument;
 import teammates.common.datatransfer.InstructorSearchResultBundle;
 import teammates.common.datatransfer.StudentSearchResultBundle;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.SearchNotImplementedException;
+import teammates.common.exception.TeammatesException;
 import teammates.common.util.Logger;
 import teammates.common.util.StringHelper;
 
@@ -15,6 +22,12 @@ import teammates.common.util.StringHelper;
  */
 public final class SearchManager {
 
+    private static final String ERROR_PUT_DOCUMENT =
+            "Failed to put document(s) %s into Solr. Root cause: %s ";
+    private static final String ERROR_SEARCH_NOT_IMPLEMENTED =
+            "Search service is not implemented";
+    private static final String STUDENT_COLLECTION_NAME = "students";
+    private static final String INSTRUCTOR_COLLECTION_NAME = "instructors";
     private static final Logger log = Logger.getLogger();
 
     private String searchServiceHost;
@@ -46,10 +59,32 @@ public final class SearchManager {
      */
     public void putStudentSearchDocuments(StudentAttributes... students) {
         if (!isSearchServiceActive()) {
-            log.severe("Search service is not implemented");
+            log.severe(ERROR_SEARCH_NOT_IMPLEMENTED);
             return;
         }
-        // TODO
+
+        SolrClient client = getSolrClient();
+
+        List<SolrInputDocument> studentDocs = new ArrayList<>();
+
+        for (StudentAttributes student : students) {
+            SolrInputDocument studentDoc = new SolrInputDocument();
+
+            studentDoc.addField("name", student.getName());
+            studentDoc.addField("email", student.getEmail());
+            studentDoc.addField("course_id", student.getCourse());
+            // TODO: add field for course name
+            studentDoc.addField("team", student.getTeam());
+            studentDoc.addField("section", student.getSection());
+
+            studentDocs.add(studentDoc);
+        }
+
+        addDocumentsToCollection(client, studentDocs, STUDENT_COLLECTION_NAME);
+    }
+
+    private ConcurrentUpdateSolrClient getSolrClient() {
+        return new ConcurrentUpdateSolrClient.Builder(searchServiceHost).build();
     }
 
     /**
@@ -57,7 +92,7 @@ public final class SearchManager {
      */
     public void deleteStudentSearchDocuments(String... keys) {
         if (!isSearchServiceActive()) {
-            log.severe("Search service is not implemented");
+            log.severe(ERROR_SEARCH_NOT_IMPLEMENTED);
             return;
         }
         // TODO
@@ -79,10 +114,43 @@ public final class SearchManager {
      */
     public void putInstructorSearchDocuments(InstructorAttributes... instructors) {
         if (!isSearchServiceActive()) {
-            log.severe("Search service is not implemented");
+            log.severe(ERROR_SEARCH_NOT_IMPLEMENTED);
             return;
         }
-        // TODO
+
+        SolrClient client = getSolrClient();
+
+        List<SolrInputDocument> instructorDocs = new ArrayList<>();
+
+        for (InstructorAttributes instructor : instructors) {
+            SolrInputDocument instructorDoc = new SolrInputDocument();
+
+            instructorDoc.addField("name", instructor.getName());
+            instructorDoc.addField("email", instructor.getEmail());
+            instructorDoc.addField("course_id", instructor.getCourseId());
+            // TODO: add field for course name
+            instructorDoc.addField("google_id", instructor.getGoogleId());
+            instructorDoc.addField("role", instructor.getRole());
+            instructorDoc.addField("displayed_name", instructor.getDisplayedName());
+
+            instructorDocs.add(instructorDoc);
+        }
+
+        addDocumentsToCollection(client, instructorDocs, INSTRUCTOR_COLLECTION_NAME);
+    }
+
+    private void addDocumentsToCollection(SolrClient client, List<SolrInputDocument> docs,
+                                          String collectionName) {
+        try {
+            client.add(collectionName, docs);
+            client.commit(collectionName);
+        } catch (SolrServerException e) {
+            log.severe(String.format(ERROR_PUT_DOCUMENT, docs, e.getRootCause())
+                    + TeammatesException.toStringWithStackTrace(e));
+        } catch (IOException e) {
+            log.severe(String.format(ERROR_PUT_DOCUMENT, docs, e.getCause())
+                    + TeammatesException.toStringWithStackTrace(e));
+        }
     }
 
     /**
@@ -90,7 +158,7 @@ public final class SearchManager {
      */
     public void deleteInstructorSearchDocuments(String... keys) {
         if (!isSearchServiceActive()) {
-            log.severe("Search service is not implemented");
+            log.severe(ERROR_SEARCH_NOT_IMPLEMENTED);
             return;
         }
         // TODO
